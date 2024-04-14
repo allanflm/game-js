6c58f9012674e6e60b82c2e93b3e8dc99d62b73f
Allan <allanfelipedk@gmail.com>
2024-04-14 08:21:41 -0300

    feat: creating the secret number variable and the hit condition

---
+++ app.js
@@ -0,0 +1,9 @@
+alert("Bem vindo ao jogo do numero secreto!")
+
+let numeroSecreto = 10;
+
+let chute = prompt("Digite um numero entre 1 a 10");
+
+if (chute == numeroSecreto) {
+    console.log("Voce acertou o numero secreto! (10) ")
+}

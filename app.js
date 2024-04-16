aefdf4c079bc663f905507123d3f927456327c87
Allan <allanfelipedk@gmail.com>
2024-04-16 07:32:03 -0300

    feat: checking whether the guessed number is greater or lesser, and using the while loop to repeat a block of code as long as a certain condition is true, and thus allowing the program to perform an action several times, also creating a counter variable to count the attempts made by the user

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 alert("Bem vindo ao jogo do numero secreto!")
 
 let numeroSecreto = 8;
-console.log(numeroSecreto)
-let chute = prompt("Digite um numero entre 1 a 10");
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-// se o chute for igual ao numero secreto
-if (chute == numeroSecreto) {
-    alert(`Voce acertou o numero secreto! -> ${numeroSecreto} `)
-} else {
-    alert("Você errou!")
+// enquanto chute for diferente de numeroSecreto;
+while (chute != numeroSecreto) {
+
+    chute = prompt("Digite um numero entre 1 a 10");
+    // se o chute for igual ao numero secreto
+    if (chute == numeroSecreto) {
+        alert(`Voce acertou o numero secreto! -> ${numeroSecreto} com ${tentativas} tentativas.`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O numero secreto é menor que ${chute}`);
+        }
+        else {
+            alert(`O numero secreto é maior que ${chute}`);
+        }
+        // tentativas = tentativas + 1;
+        tentativas++;
+    }
 }

345000d742d133c3994c19280ce900c7f54831c7
Allan <allanfelipedk@gmail.com>
2024-04-18 19:54:31 -0300

    feat: using Math.random to randomly generate the secret number

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript
+
 alert("Bem vindo ao jogo do numero secreto!")
 
-let numeroSecreto = 8;
+let numeroSecreto = parseInt(Math.random() * 100 + 1)
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
@@ -8,9 +10,10 @@ let tentativas = 1;
 // enquanto chute for diferente de numeroSecreto;
 while (chute != numeroSecreto) {
 
-    chute = prompt("Digite um numero entre 1 a 10");
+    chute = prompt("Digite um numero entre 1 a 100");
     // se o chute for igual ao numero secreto
     if (chute == numeroSecreto) {
+        break;
         alert(`Voce acertou o numero secreto! -> ${numeroSecreto} com ${tentativas} tentativas.`);
     } else {
         if (chute > numeroSecreto) {
@@ -23,3 +26,14 @@ while (chute != numeroSecreto) {
         tentativas++;
     }
 }
+// operador ternario
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+
+alert(`Voce acertou o numero secreto! -> ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+// if (tentativas > 1) {
+//     alert(`Voce acertou o numero secreto! -> ${numeroSecreto} com ${tentativas} tentativas.`);
+// }
+// else {
+//     alert(`Voce acertou o numero secreto! -> ${numeroSecreto} com ${tentativas} tentativa.`);
+// }

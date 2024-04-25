64a0bc36642330e6519ab8d44b31422b35af2c86
Allan <allanfelipedk@gmail.com>
2024-04-24 21:35:03 -0300

    feat: creating functions to generate the correct number and compare with the user's gues

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = "Jogo do número secreto.";
+let numeroSecreto = gerarNumeroAleatorio();
 
 let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = "Escolha um numero de 1 a 10."
+paragrafo.innerHTML = "Escolha um numero de 1 a 10.";
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela("h1", "Jogo do numero secreto.");
+exibirTextoNaTela("p", "Escolha um numero entre 1 a 10.")
+
 
 function verificarChute() {
-    console.log("O botao foi clicado!");
+    let chute = document.querySelector("input").value;
+    console.log(chute == numeroSecreto);
+    console.log(numeroSecreto);
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
 }
